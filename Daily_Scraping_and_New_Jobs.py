fc6fbfdc525fe4d45d5c02133ff2809b1254bb15
shikhar-tern <151713807+shikhar-tern@users.noreply.github.com>
2023-12-18 20:46:38 +0530

    Update Daily_Scraping_and_New_Jobs.py

--- Daily_Scraping_and_New_Jobs.py
+++ Daily_Scraping_and_New_Jobs.py
@@ -864,7 +864,7 @@ master_final = master_df(new_job,df_jd)
 
 
 master_push_to_s3('master_new_job_data','master_new_job',str(date.today()))
-
+#----------------------------------------------------------------------------------------------------------------------------------
 
 print('Starting with Creating Masters')
 # #### Listing Page Master
@@ -1021,7 +1021,7 @@ def merged_master_df(x):
 
 
 merged_master = merged_master_df('master_new_job_data')
-
+del merged_master
 
 # #### New Jobs Master
 
@@ -1053,7 +1053,7 @@ def new_jobs_master_df(x):
 
 
 new_jobs_master = new_jobs_master_df('new_job_data')
-
+del new_jobs_master
 
 # #### Job Description Master
 
@@ -1171,4 +1171,291 @@ def jd_master_df(a,b):
 
 
 jd_master = jd_master_df('job_information_updated','jd_page_data')
+
+### Update Code
+jd_master['scraped_date'] = pd.to_datetime(jd_master['scraped_date'])
+listing_page_master['scrap_date'] = pd.to_datetime(listing_page_master['scrap_date'])
+
+# Assuming 'closing_date' is the column with date strings
+listing_page_master['closing_date'] = pd.to_datetime(
+    listing_page_master['closing_date'], 
+    infer_datetime_format=True, 
+    errors='coerce')
+
+# Convert the datetime column to the desired 'yyyy-mm-dd' format
+listing_page_master['closing_date'] = pd.to_datetime(listing_page_master['closing_date'].dt.strftime('%Y-%m-%d'))
+
+# Assuming 'closing_date' is the column with date strings
+jd_master['date_posted'] = pd.to_datetime(
+    jd_master['date_posted'], 
+    infer_datetime_format=True, 
+    errors='coerce')
+
+# Convert the datetime column to the desired 'yyyy-mm-dd' format
+jd_master['date_posted'] = pd.to_datetime(jd_master['date_posted'].dt.strftime('%Y-%m-%d'))
+
+#there are two types
+#1. job found
+#2. job not found or closed
+
+#check and flags for 
+#1.flag for job active or not (2 columns - active_not_active flag and how_many_days_to_close)
+## active and not active flag
+#1.1 if active then in how many days the job is going to get closed
+#1.2 if not active then flag it as not active
+
+#2.flag change for salary (3 columns - salary_same_or_not,salary_changed_to,salary_changed_when)
+## salary same or not flag
+#2.1 if salary changed then when it changed and what it changed to
+#2.2 if salary same then flag it as not changed
+
+#3.Latested date of update (latest_job_info_update)
+#3.1 when was job information updated last
+
+def active_inactive(x):
+    if x.days <= 0 :
+        return 'closed'
+    else:
+        return 'active'
+    
+def update_information(link):
+    print(f"Started with: {link}")
+    job_lists = list(listing_page_master['short_job_link'].unique())
+    a = listing_page_master[listing_page_master['short_job_link']==link].sort_values('scrap_date')
+    b = jd_master[jd_master['short_job_link']==link].sort_values('scraped_date')
+    
+    if len(a) == 1  and (list(a['scrap_date'])[0] in list(b['scraped_date'])):
+        c = a[['Role','salary','closing_date', 'job_code', 'short_job_link','scrap_date']].merge(b[['scraped_date','date_posted']],left_on='scrap_date',right_on='scraped_date',how='outer') #<---- check how
+        c['Role'] = c['Role'].fillna(method='ffill')
+        c['salary'] = c['salary'].fillna(method='ffill')
+        c['closing_date'] = c['closing_date'].fillna(method='ffill')
+        c['job_code'] = c['job_code'].fillna(method='ffill')
+        c['short_job_link'] = c['short_job_link'].fillna(method='ffill')
+        c['scrap_date'] = c['scrap_date'].fillna(method='ffill')
+
+        # Assuming df is your DataFrame
+        c['scraped_date'] = c['scraped_date'].fillna(method='ffill')
+        c['date_posted'] = c['date_posted'].fillna(method='ffill')
+
+        c['Today_Date'] = str(date.today())
+        c['Today_Date'] = pd.to_datetime(c['Today_Date'])
+
+        #days to close
+        c['Days_to_close'] = c['closing_date'] - c['Today_Date']
+
+        #job_active_inactive_flag
+        c['active_inactive'] = c['Days_to_close'].apply(lambda x: active_inactive(x))
+
+        #Latested date of update
+        c['Updated_on'] = str(max(c['scraped_date']).date())
+
+        c['salary_changed'] = c['salary'] != c['salary'].shift(1)
+
+        c['latest_salary'] = c['salary'].where(c['salary_changed'], None)
+
+        c['salary_change_date'] = c['scrap_date'].where(c['salary_changed'], pd.NaT)
+
+        # Fill NaN values in 'latest_salary' and 'salary_change_date'
+        c['latest_salary'] = c['latest_salary'].ffill()
+        c['salary_change_date'] = c['salary_change_date'].ffill()
+
+        c = c.drop(columns=['salary_changed'])
+
+        print(f"{job_lists.index(link)} done out of {len(job_lists)}")
+
+        return c
+
+    elif (len(a)==1) and (list(a['scrap_date'])[0] not in list(b['scraped_date'])):
+        c = a[['Role','salary','closing_date', 'job_code', 'short_job_link','scrap_date']].merge(b[['scraped_date','date_posted']],left_on='scrap_date',right_on='scraped_date',how='outer') #<---- check how
+        c['Role'] = c['Role'].fillna(method='ffill')
+        c['salary'] = c['salary'].fillna(method='ffill')
+        c['closing_date'] = c['closing_date'].fillna(method='ffill')
+        c['job_code'] = c['job_code'].fillna(method='ffill')
+        c['short_job_link'] = c['short_job_link'].fillna(method='ffill')
+        c['scrap_date'] = c['scrap_date'].fillna(method='ffill')
+
+        # Assuming df is your DataFrame
+        c['scraped_date'] = c['scraped_date'].fillna(c['scrap_date'])
+        c['date_posted'] = c['date_posted'].fillna(method='bfill')
+
+        c['Today_Date'] = str(date.today())
+        c['Today_Date'] = pd.to_datetime(c['Today_Date'])
+
+        #days to close
+        c['Days_to_close'] = c['closing_date'] - c['Today_Date']
+
+        #job_active_inactive_flag
+        c['active_inactive'] = c['Days_to_close'].apply(lambda x: active_inactive(x))
+
+        #Latested date of update
+        c['Updated_on'] = str(max(c['scraped_date']).date())
+
+        c['salary_changed'] = c['salary'] != c['salary'].shift(1)
+
+        c['latest_salary'] = c['salary'].where(c['salary_changed'], None)
+
+        c['salary_change_date'] = c['scrap_date'].where(c['salary_changed'], pd.NaT)
+
+        # Fill NaN values in 'latest_salary' and 'salary_change_date'
+        c['latest_salary'] = c['latest_salary'].ffill()
+        c['salary_change_date'] = c['salary_change_date'].ffill()
+
+        c = c.drop(columns=['salary_changed'])
+
+        print(f"{job_lists.index(link)} done out of {len(job_lists)}")
+
+        return c
+
+    elif (len(a)>1) and (len(b)==0):
+        c = a[['Role','salary','closing_date', 'job_code', 'short_job_link','scrap_date']].merge(b[['scraped_date','date_posted']],left_on='scrap_date',right_on='scraped_date',how='left') #<---- check how
+        # Assuming df is your DataFrame
+        c['scraped_date'] = c['scraped_date'].fillna(method='ffill')
+        c['date_posted'] = c['date_posted'].fillna(method='ffill')
+
+        c['Today_Date'] = str(date.today())
+        c['Today_Date'] = pd.to_datetime(c['Today_Date'])
+
+        #days to close
+        c['Days_to_close'] = c['closing_date'] - c['Today_Date']
+
+        #job_active_inactive_flag
+        c['active_inactive'] = c['Days_to_close'].apply(lambda x: active_inactive(x))
+
+        #Latested date of update
+        c['Updated_on'] = str(max(c['scrap_date']).date())
+
+        c['salary_changed'] = c['salary'] != c['salary'].shift(1)
+
+        c['latest_salary'] = c['salary'].where(c['salary_changed'], None)
+
+        c['salary_change_date'] = c['scrap_date'].where(c['salary_changed'], pd.NaT)
+
+        # Fill NaN values in 'latest_salary' and 'salary_change_date'
+        c['latest_salary'] = c['latest_salary'].ffill()
+        c['salary_change_date'] = c['salary_change_date'].ffill()
+
+        c = c.drop(columns=['salary_changed'])
+
+        print(f"{job_lists.index(link)} done out of {len(job_lists)}")
+
+        return c
+
+    elif (len(a)>1) and (any(elem in list(b['scraped_date']) for elem in list(a['scrap_date']))):
+        c = a[['Role','salary','closing_date', 'job_code', 'short_job_link','scrap_date']].merge(b[['scraped_date','date_posted']],left_on='scrap_date',right_on='scraped_date',how='left') #<---- check how
+        # Assuming df is your DataFrame
+        c['scraped_date'] = c['scraped_date'].fillna(c['scrap_date'])
+        c['date_posted'] = c['date_posted'].fillna(method='bfill')
+
+        c['Today_Date'] = str(date.today())
+        c['Today_Date'] = pd.to_datetime(c['Today_Date'])
+
+        #days to close
+        c['Days_to_close'] = c['closing_date'] - c['Today_Date']
+
+        #job_active_inactive_flag
+        c['active_inactive'] = c['Days_to_close'].apply(lambda x: active_inactive(x))
+
+        #Latested date of update
+        c['Updated_on'] = str(max(c['scraped_date']).date())
+
+        c['salary_changed'] = c['salary'] != c['salary'].shift(1)
+
+        c['latest_salary'] = c['salary'].where(c['salary_changed'], None)
+
+        c['salary_change_date'] = c['scrap_date'].where(c['salary_changed'], pd.NaT)
+
+        # Fill NaN values in 'latest_salary' and 'salary_change_date'
+        c['latest_salary'] = c['latest_salary'].ffill()
+        c['salary_change_date'] = c['salary_change_date'].ffill()
+
+        c = c.drop(columns=['salary_changed'])
+
+        print(f"{job_lists.index(link)} done out of {len(job_lists)}")
+
+        return c
+
+    elif (len(a)>1) and (len(b)>=1) and (any(elem not in list(b['scraped_date']) for elem in list(a['scrap_date']))):
+        c = a[['Role','salary','closing_date', 'job_code', 'short_job_link','scrap_date']].merge(b[['scraped_date','date_posted']],left_on='scrap_date',right_on='scraped_date',how='outer') #<---- check how
+
+        c['Role'] = c['Role'].fillna(method='ffill')
+        c['salary'] = c['salary'].fillna(method='ffill')
+        c['closing_date'] = c['closing_date'].fillna(method='ffill')
+        c['job_code'] = c['job_code'].fillna(method='ffill')
+        c['short_job_link'] = c['short_job_link'].fillna(method='ffill')
+        c['scrap_date'] = c['scrap_date'].fillna(method='ffill')
+        # Assuming df is your DataFrame
+        c['scraped_date'] = c['scraped_date'].fillna(c['scrap_date'])
+        c['date_posted'] = c['date_posted'].fillna(method='bfill')
+
+        c['Today_Date'] = str(date.today())
+        c['Today_Date'] = pd.to_datetime(c['Today_Date'])
+
+        #days to close
+        c['Days_to_close'] = c['closing_date'] - c['Today_Date']
+
+        #job_active_inactive_flag
+        c['active_inactive'] = c['Days_to_close'].apply(lambda x: active_inactive(x))
+
+        #Latested date of update
+        c['Updated_on'] = str(max(c['scraped_date']).date())
+
+        c['salary_changed'] = c['salary'] != c['salary'].shift(1)
+
+        c['latest_salary'] = c['salary'].where(c['salary_changed'], None)
+
+        c['salary_change_date'] = c['scrap_date'].where(c['salary_changed'], pd.NaT)
+
+        # Fill NaN values in 'latest_salary' and 'salary_change_date'
+        c['latest_salary'] = c['latest_salary'].ffill()
+        c['salary_change_date'] = c['salary_change_date'].ffill()
+
+        c = c.drop(columns=['salary_changed'])
+
+        print(f"{job_lists.index(link)} done out of {len(job_lists)}")
+
+        return c
+
+def process_link(link):
+    new_update = update_information(link)
+    return new_update, new_update.tail(1)
+
+start_time = time.time()
+print(str(datetime.now()))
+job_updates = pd.DataFrame()
+latest_job_updates = pd.DataFrame()
+if __name__ == '__main__':
+    # Define the URLs
+    job_lists = list(listing_page_master['short_job_link'].unique())
+
+    results_list = []
+    
+    with concurrent.futures.ThreadPoolExecutor(max_workers=mp.cpu_count()-1) as executor:
+        # Submit the scraping function to the executor for each page number
+        results = list(executor.map(process_link, job_lists))
+        
+        for i, (new_update, latest_update) in enumerate(results):
+            if new_update is not None and latest_update is not None:
+                job_updates = pd.concat([job_updates, new_update], axis=0, ignore_index=True)
+                latest_job_updates = pd.concat([latest_job_updates, latest_update], axis=0, ignore_index=True)
+                print(f"Result {i + 1} processed and appended")
+            else:
+                print(f"Result {i + 1} is None. Skipping...")
+
+    job_updates.to_csv(r"/home/ec2-user/scrape_data/master_data/Job_Information_Full_Updated_Master.csv",index=False)
+    latest_job_updates.to_csv(r"/home/ec2-user/scrape_data/master_data/Latest_Updated_Master.csv",index=False)
+    jd_latest = jd_master[jd_master['scraped_date']==max(jd_master['scraped_date'])]
+    active_job = latest_job_updates[latest_job_updates['active_inactive']=='active']
+    df_fin = active_job.merge(jd_latest[['job_summary', 'job_discription', 'employer_name',
+       'employer_address', 'employer_post_code', 'employer_website',
+       'contact_person_position', 'contact_person_name',
+       'contact_person_email', 'contact_person_number','short_job_link']],on ='short_job_link',how='left')
+    df_fin.to_csv(r"/home/ec2-user/scrape_data/master_data/Active_Jobs.csv",index=False)
+
+    push_to_s3("master_data","Job_Information_Full_Updated_Master")
+    push_to_s3("master_data","Latest_Updated_Master")  
+    push_to_s3("master_data","Active_Jobs")        
+end_time = time.time()
+duration = end_time - start_time
+print(f"Time taken: {duration/60} mintues")
+
 
