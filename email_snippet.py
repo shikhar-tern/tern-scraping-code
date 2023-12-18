e9642229119eef29ae9384661bbbe1550704c644
shikhar-tern <151713807+shikhar-tern@users.noreply.github.com>
2023-12-18 20:00:34 +0530

    Create email_snippet.py

---
+++ email_snippet.py
@@ -0,0 +1,129 @@
+import sys
+import pandas as pd
+import requests
+from requests.exceptions import ConnectionError, ReadTimeout
+import json
+from bs4 import BeautifulSoup
+from lxml import html
+import time
+import re
+from datetime import date
+from datetime import datetime
+from datetime import timedelta
+import multiprocessing as mp
+import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor
+import time
+import threading
+
+from io import StringIO
+import json
+
+import os
+import glob
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+from email.message import EmailMessage
+import ssl
+
+import boto3
+from botocore.exceptions import ClientError
+import botocore
+import s3fs as s3
+
+# ##### email new jobs
+
+# In[69]:
+
+
+# # Specify the path to your JSON file
+# file_path = r"/home/ec2-user/tern-scraping-code/email_credentials.json"
+
+# # Read the JSON file and convert it to a dictionary
+# with open(file_path, 'r') as json_file:
+#     email_cred = json.load(json_file)
+
+
+# # In[70]:
+
+
+# # Define a function to apply color and style formatting to the cells
+# def format_row(row):
+#     return f"""
+#         <tr>
+#             <td style="padding: 12px 15px; background-color: {'#D6EEEE' if row['New Jobs Count'] % 2 == 0 else '#D6EEEE'}; border: 2px solid #ddd; font-weight: {'bold' if row.name == 'Keywords' else 'normal'}; text-align: center;">{row['Keywords']}</td>
+#             <td style="padding: 12px 15px; background-color: {'#D6EEEE' if row['New Jobs Count'] % 2 == 0 else '#D6EEEE'}; border: 2px solid #ddd; font-weight: {'bold' if row.name == 'Keywords' else 'normal'}; text-align: center;">{row['New Jobs Count']}</td>
+#         </tr>
+#     """
+
+
+# # In[31]:
+
+
+# def email_people(email_cred,master_final):
+#     # Load your DataFrame from the CSV file
+#     new_csv_file_path = r"/home/ec2-user/scrape_data/master_new_job_data/master_new_job_{}.xlsx".format(str(date.today()))
+#     # Email configuration
+#     sender_email = email_cred['email']
+#     sender_password = email_cred['password']
+#     receiver_email = ["shikharrajput@gmail.com","safal.verma@tern-group.com"]
+# #     receiver_email = ["shikharrajput@gmail.com","safal.verma@tern-group.com","ashita@tern-group.com","akshay.rao@tern-group.com"]
+
+#     subject = f"New Job on {str(date.today())}"
+    
+#     # Create the email body with a formatted table
+#     body = f"""
+#     <html>
+#     <body>
+#       <p>Hi,</p>
+
+#       <p> As of today {str(date.today())} there are {master_final.shape[0]} new jobs on NHS </p>
+
+#       <p>PFA.</p>
+
+#       <p>Best regards,<br>Your Name</p>
+#     </body>
+#     </html>
+#     """
+    
+#     # Create the email message
+#     message = MIMEMultipart()
+#     message.attach(MIMEText(body, 'html'))
+    
+#     # Attach the new CSV file
+#     with open(new_csv_file_path, 'rb') as attachment:
+#         part = MIMEBase('application', 'octet-stream')
+#         part.set_payload(attachment.read())
+#         encoders.encode_base64(part)
+#         part.add_header('Content-Disposition', f'attachment; filename=New_Job_{str(date.today())}')
+#         message.attach(part)
+    
+#     # Create the email message
+#     message['From'] = sender_email
+#     message['To'] = ", ".join(receiver_email)
+#     message['Subject'] = subject
+
+#     context = ssl.create_default_context()
+
+#     # Connect to the SMTP server and send the email
+#     smtp_server = "smtp.gmail.com"
+#     smtp_port = 465
+
+#     with smtplib.SMTP_SSL(smtp_server, smtp_port, context=context) as server:
+#         server.login(sender_email, sender_password)
+#         server.send_message(message)
+
+#     print("Email sent successfully!")
+
+
+# # In[32]:
+
+
+# email_people(email_cred,master_final)
+
+
+# In[ ]:
