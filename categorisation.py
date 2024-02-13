347f474f76b1e284099cae9f63c7318e0128dc41
shikhar-tern <151713807+shikhar-tern@users.noreply.github.com>
2024-02-14 02:42:01 +0530

    Update categorisation.py

--- categorisation.py
+++ categorisation.py
@@ -44,12 +44,12 @@ def active_jobs_df(x):
     return active_jobs_df
 
 def specialisation_list_df_list(x,y):
-    df = pd.read_excel(r"/home/ec2-user/tern-scraping-code/{}.xlsx".format(x),sheet_name=y)
+    df = pd.read_excel(r"/home/ec2-user/scrape_data/master_data/{}.xlsx".format(x),sheet_name=y)
     df_list = list(df[y].unique())
     return df_list
 
 def specialisation_list_df(x,y):
-    df = pd.read_excel(r"/home/ec2-user/tern-scraping-code/{}.xlsx".format(x),sheet_name=y)
+    df = pd.read_excel(r"/home/ec2-user/scrape_data/master_data/{}.xlsx".format(x),sheet_name=y)
     return df
 
 def push_to_s3(x,y):
