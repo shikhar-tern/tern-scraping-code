696682396daa20cb9e059d1406d560d7c33352c5
shikhar-tern <151713807+shikhar-tern@users.noreply.github.com>
2024-02-23 13:20:19 +0530

    Update testing_s3_connect_and_push_data.py

--- testing_s3_connect_and_push_data.py
+++ testing_s3_connect_and_push_data.py
@@ -127,9 +127,9 @@ def jd_data_list(x):
     flat_list = [item for sublist in files_in_s3 for item in sublist]
     filtered_list = [item for item in flat_list if item != '']
     prefixed_list = [f'{x}/' + item for item in filtered_list]
-    # pattern = re.compile(r'/job_information_updated_\d{4}-\d{2}-\d{2}\.csv$')
-    # filtered_list_2 = [element for element in prefixed_list if pattern.search(element)]
-    return prefixed_list
+    pattern = re.compile(r'/job_information_updated_\d{4}-\d{2}-\d{2}\.csv$')
+    filtered_list_2 = [element for element in prefixed_list if pattern.search(element)]
+    return filtered_list_2
 
 
 def push_to_s3(x,y):
@@ -142,3 +142,35 @@ def push_to_s3(x,y):
 specific_files = jd_data_list("job_information_updated")
 print(specific_files)
 print(len(specific_files))
+
+jd_master = pd.DataFrame()
+for file in specific_files:
+    print(f'Starting with: {file}')
+    s3 = boto3.resource("s3")
+    #load from bucket
+    obj = s3.Bucket('nhs-dataset').Object(file).get()
+    dd = pd.read_csv(obj['Body'])
+    dd['job_url'] = dd['job_url_hit']
+    df_not_null = dd[(dd['job_url_hit'].notnull())]
+    df_is_null = dd[(dd['job_url_hit'].isnull())]
+    df_is_null['job_url_hit'] = 'https://www.jobs.nhs.uk/candidate/jobadvert/' + df_is_null['job_reference_number']
+    dd = pd.concat([df_not_null,df_is_null],axis=0,ignore_index=True)
+    dd['job_url_hit'] = dd['job_url'].apply(lambda x: remove_keyword_param(x))
+    dd['job_code'] = dd['job_url_hit'].apply(lambda x:extract_job_codes_(x))
+    dd['short_job_link'] = dd['job_url_hit'].apply(lambda x:short_link(x))
+    dd['job_reference_number'] = dd['job_reference_number'].apply(lambda x: fixing_job_ref(x))
+    dd = dd.drop_duplicates(['scraped_date','job_url_hit'],keep='first').reset_index(drop=True)
+    jd_master = pd.concat([jd_master,dd],axis=0,ignore_index=True)
+    print(f'Done with: {file}')
+
+jd_master.to_csv(r"/home/ec2-user/scrape_data/job_information_updated/job_information_updated_{}.csv".format(str(date.today())),index=False)
+
+def jd_page_push_to_s3(x,y):
+    print(f'Pushing {y} to s3 bucket in {x}')
+    s3 = boto3.resource(service_name = 's3', region_name = 'eu-west-2')
+    df = pd.read_csv(f"/home/ec2-user/scrape_data/{x}/{y}.csv")
+    #push to bucket
+    s3.Bucket('nhs-dataset').upload_file(Filename = f'/home/ec2-user/scrape_data/{x}/{y}.csv',Key = f'{x}/{y}.csv')
+    print(f'{y} pushed to bucket in {x}')
+
+jd_page_push_to_s3("job_information_updated",f"job_information_updated_{str(date.today())}")
