bc5546f800770d8076fdff0fba2c51cc25b381e9
shikhar-tern <151713807+shikhar-tern@users.noreply.github.com>
2024-02-25 16:02:52 +0530

    Update delete_file.py

--- delete_file.py
+++ delete_file.py
@@ -3,9 +3,7 @@ dir = '/home/ec2-user/scrape_data'
 files = os.listdir(dir)
 new_list = []
 for i in files:
-    if i == 'master_data':
-        pass
-    elif i == 'log.txt':
+    if i == 'log.txt':
         pass
     else:
         new_list.append(i)
@@ -14,8 +12,11 @@ def del_files(dir,x):
     new_path = dir + '/' + x
     files_list = os.listdir(new_path)
     for j in files_list:
-        #print(dir+"/"+x+"/"+j)
-        os.remove(dir+"/"+x+"/"+j)
+        if j in ['Listing_Page_Master.csv','Active_Jobs_with_categorisation.xlsx','Jobs_Information_Master.csv','Final_Speciality.xlsx']:
+            pass
+        else:
+            # print(dir+"/"+x+"/"+j)
+            os.remove(dir+"/"+x+"/"+j)
 
 for x in new_list:
     del_files(dir,x)
