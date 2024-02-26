3d55f8a052b8ba28402aa8fa3273e4f693116777
shikhar-tern <151713807+shikhar-tern@users.noreply.github.com>
2024-02-26 18:50:40 +0530

    Update Daily_Scraping_and_New_Jobs.py

--- Daily_Scraping_and_New_Jobs.py
+++ Daily_Scraping_and_New_Jobs.py
@@ -1636,10 +1636,13 @@ def final_checks(active_jobs,final_speciality,nurse_final_speciality,ahp_df):
     active_jobs_final_2['Major Specialisation'].fillna("",inplace=True)
     active_jobs_final_2['Specialisation'].fillna("",inplace=True)
     active_jobs_final_2.loc[(active_jobs_final_2['Final_Tag']=='') & ((active_jobs_final_2['Role'].str.lower().str.strip().str.contains('doctor')) | active_jobs_final_2['Role'].str.lower().str.strip().str.contains('doctors')),'Final_Tag'] = 'Doctor'
+    active_jobs_final_2.to_csv(r"/home/ec2-user/scrape_data/master_data/Active_Jobs_with_categorisation.csv")
     active_jobs_final_2.to_excel(r"/home/ec2-user/scrape_data/master_data/Active_Jobs_with_categorisation.xlsx")
     push_to_s3_categorisation_file('master_data','Active_Jobs_with_categorisation')
+    push_to_s3('master_data','Active_Jobs_with_categorisation')
     email_people(email_cred,"Active Jobs with categorisation and all other Files")
     delete_files("master_data","Active_Jobs_with_categorisation.xlsx")
+    delete_files("master_data","Active_Jobs_with_categorisation.csv")
     return active_jobs_final_2
 
 active_jobs_final_2 = final_checks(active_jobs,final_speciality,nurse_final_speciality,ahp_df)
