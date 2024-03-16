db08869c78bb6aa8c37ea82d290225c1104cc490
shikhar-tern <151713807+shikhar-tern@users.noreply.github.com>
2024-03-16 12:57:30 +0530

    Create pushing_df_to_postgresql.py
    testing for creating connection to postgresql

---
+++ pushing_df_to_postgresql.py
@@ -0,0 +1,27 @@
+# sudo -u postgres psql -> CONNECT TO POSTGRESQL ON SERVER
+import pandas as pd
+import psycopg2
+import json
+
+# Specify the path to your JSON file
+file_path = r"/home/ec2-user/tern-scraping-code/postgresql_credentials.json"
+
+# Read the JSON file and convert it to a dictionary
+with open(file_path, 'r') as json_file:
+    conn_cred = json.load(json_file)
+
+
+def establising_connection(x,conn_cred):
+    # Connect to the PostgreSQL database
+    conn = psycopg2.connect(
+        database=x,
+        user=conn_cred['user'],
+        password=conn_cred['password'],
+        host=conn_cred['host'],
+        port=conn_cred['port']
+    )
+    print("Connection Established")
+
+    print("Going to Close Connection")
+    # Close the cursor and connection
+    conn.close()
