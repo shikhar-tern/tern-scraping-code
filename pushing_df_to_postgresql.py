4b9f742c0c83a376e421c315439219a4cb3604bd
shikhar-tern <151713807+shikhar-tern@users.noreply.github.com>
2024-03-16 13:10:39 +0530

    Update pushing_df_to_postgresql.py

--- pushing_df_to_postgresql.py
+++ pushing_df_to_postgresql.py
@@ -12,18 +12,32 @@ with open(file_path, 'r') as json_file:
 
 
 def establising_connection(x,conn_cred):
-    # Connect to the PostgreSQL database
-    conn = psycopg2.connect(
-        database=x,
-        user=conn_cred['user'],
-        password=conn_cred['password'],
-        host=conn_cred['host'],
-        port=conn_cred['port']
-    )
-    print("Connection Established")
+    try:
+        # Connect to your postgres DB
+        conn = psycopg2.connect(
+            dbname=x,
+            user=conn_cred['user'],
+            password=conn_cred['password'],
+            host=conn_cred['host'],
+            port=conn_cred['port']
+        )
+        # # Open a cursor to perform database operations
+        # with conn.cursor() as cursor:
+        #     # Execute a query
+        #     cursor.execute("SELECT version();")
 
-    print("Going to Close Connection")
-    # Close the cursor and connection
-    conn.close()
+        #     # Retrieve query results
+        #     records = cursor.fetchone()
+        #     print(f"Connected to - {records}")
+        print("Connection Established")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
+    finally:
+        print("Close Connection")
+        # Close connection
+        if conn:
+            conn.close()
+            
 establising_connection('scrappydb',conn_cred)
+
